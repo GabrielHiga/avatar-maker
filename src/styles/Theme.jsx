15bb84b4e9727154fa0bb7158a2b1d8d1ef31b6b
Hideki <gabriel_hideki@live.com>
2023-06-03 02:00:51 -0300

    Finally it's working \o/

--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -1,5 +1,78 @@
+//import Skin
+import imgSkin        from '../assets/skins/skin.png';
+import imgSkin_02     from '../assets/skins/skin_02.png';
+import imgSkin_03     from '../assets/skins/skin_03.png';
+import imgSkin_04     from '../assets/skins/skin_04.png';
+import imgSkin_05     from '../assets/skins/skin_05.png';
+import imgSkin_06     from '../assets/skins/skin_06.png';
 
-import teste from '../assets/skins/skin_05.png';
+//import Clothes
+import imgClothes_01 from '../assets/clothes/cloth_01.png';
+import imgClothes_02 from '../assets/clothes/cloth_02.png';
+import imgClothes_03 from '../assets/clothes/cloth_03.png';
+import imgClothes_04 from '../assets/clothes/cloth_04.png';
+import imgClothes_05 from '../assets/clothes/cloth_05.png';
+import imgClothes_06 from '../assets/clothes/cloth_06.png';
+import imgClothes_07 from '../assets/clothes/cloth_07.png';
+import imgClothes_08 from '../assets/clothes/cloth_08.png';
+import imgClothes_09 from '../assets/clothes/cloth_09.png';
+import imgClothes_10 from '../assets/clothes/cloth_10.png';
+import imgClothes_11 from '../assets/clothes/cloth_11.png';
+import imgClothes_12 from '../assets/clothes/cloth_12.png';
+import imgClothes_13 from '../assets/clothes/cloth_13.png';
+import imgClothes_14 from '../assets/clothes/cloth_14.png';
+
+//import Mounth
+import imgMounth_01 from '../assets/mounth/mounth_01.png';
+import imgMounth_02 from '../assets/mounth/mounth_02.png';
+import imgMounth_03 from '../assets/mounth/mounth_03.png';
+import imgMounth_04 from '../assets/mounth/mounth_04.png';
+import imgMounth_05 from '../assets/mounth/mounth_05.png';
+import imgMounth_06 from '../assets/mounth/mounth_06.png';
+import imgMounth_07 from '../assets/mounth/mounth_07.png';
+import imgMounth_08 from '../assets/mounth/mounth_08.png';
+
+//import eyes
+import imgEyes_01 from '../assets/eyes/eyes_01.png';
+import imgEyes_02 from '../assets/eyes/eyes_02.png';
+import imgEyes_03 from '../assets/eyes/eyes_03.png';
+import imgEyes_04 from '../assets/eyes/eyes_04.png';
+import imgEyes_05 from '../assets/eyes/eyes_05.png';
+import imgEyes_06 from '../assets/eyes/eyes_06.png';
+import imgEyes_07 from '../assets/eyes/eyes_07.png';
+
+//import head
+import imgHead_01 from '../assets/head/head_01.png';
+import imgHead_02 from '../assets/head/head_02.png';
+import imgHead_03 from '../assets/head/head_03.png';
+import imgHead_04 from '../assets/head/head_04.png';
+import imgHead_05 from '../assets/head/head_05.png';
+import imgHead_06 from '../assets/head/head_06.png';
+import imgHead_07 from '../assets/head/head_07.png';
+import imgHead_08 from '../assets/head/head_08.png';
+import imgHead_09 from '../assets/head/head_09.png';
+import imgHead_10 from '../assets/head/head_10.png';
+import imgHead_11 from '../assets/head/head_11.png';
+import imgHead_12 from '../assets/head/head_12.png';
+import imgHead_13 from '../assets/head/head_13.png';
+import imgHead_14 from '../assets/head/head_14.png';
+import imgHead_15 from '../assets/head/head_15.png';
+import imgHead_16 from '../assets/head/head_16.png';
+import imgHead_17 from '../assets/head/head_17.png';
+import imgHead_18 from '../assets/head/head_18.png';
+import imgHead_19 from '../assets/head/head_19.png';
+import imgHead_20 from '../assets/head/head_20.png';
+import imgHead_21 from '../assets/head/head_21.png';
+import imgHead_22 from '../assets/head/head_22.png';
+
+//import examples from hero
+import imgExamples_01 from '../assets/ex_01.png';
+import imgExamples_02 from '../assets/ex_02.png';
+import imgExamples_03 from '../assets/ex_03.png';
+import imgExamples_04 from '../assets/ex_04.png';
+import imgExamples_05 from '../assets/ex_05.png';
+// import imgExamples_06 from '../assets/ex_06.png';
+
 
 export const Theme =  {
     colors: {
@@ -9,81 +82,78 @@ export const Theme =  {
     },
     items: {
         skins: [
-            { id: 0, item: teste, },
-            { id: 1, item: teste, },
-            { id: 2, item: teste, },
-            { id: 3, item: teste, },
-            { id: 4, item: teste, },
-            { id: 5, item:'src/assets/skins/skin_05.png', },
-            { id: 6, item:'src/assets/skins/skin_06.png',
-        }
+            { id: 0, item: imgSkin, },
+            { id: 1, item: imgSkin_02, },
+            { id: 2, item: imgSkin_03, },
+            { id: 3, item: imgSkin_04, },
+            { id: 4, item: imgSkin_05, },
+            { id: 5, item: imgSkin_06, },
         ],
         clothes: [
-            { id: 0, item: 'src/assets/clothes/cloth_01.png', },
-            { id: 1, item: 'src/assets/clothes/cloth_02.png', },
-            { id: 2, item: 'src/assets/clothes/cloth_03.png', },
-            { id: 3, item: 'src/assets/clothes/cloth_04.png', },
-            { id: 4, item: 'src/assets/clothes/cloth_05.png', },
-            { id: 5, item: 'src/assets/clothes/cloth_06.png', },
-            { id: 6, item: 'src/assets/clothes/cloth_07.png', },
-            { id: 7, item: 'src/assets/clothes/cloth_08.png', },
-            { id: 8, item: 'src/assets/clothes/cloth_09.png', },
-            { id: 9, item: 'src/assets/clothes/cloth_10.png', },
-            { id: 10, item: 'src/assets/clothes/cloth_11.png', },
-            { id: 11, item: 'src/assets/clothes/cloth_12.png', },
-            { id: 12, item: 'src/assets/clothes/cloth_13.png', },
-            { id: 13, item: 'src/assets/clothes/cloth_14.png', }
+            { id: 0, item: imgClothes_01, },
+            { id: 1, item: imgClothes_02, },
+            { id: 2, item: imgClothes_03, },
+            { id: 3, item: imgClothes_04, },
+            { id: 4, item: imgClothes_05, },
+            { id: 5, item: imgClothes_06, },
+            { id: 6, item: imgClothes_07, },
+            { id: 7, item: imgClothes_08, },
+            { id: 8, item: imgClothes_09, },
+            { id: 9, item: imgClothes_10, },
+            { id: 10, item: imgClothes_11, },
+            { id: 11, item: imgClothes_12, },
+            { id: 12, item: imgClothes_13, },
+            { id: 13, item: imgClothes_14, }
         ],
         mounth : [
-            { id: 0, item: 'src/assets/mounth/mounth_01.png' },
-            { id: 1, item: 'src/assets/mounth/mounth_02.png' },
-            { id: 2, item: 'src/assets/mounth/mounth_03.png' },
-            { id: 3, item: 'src/assets/mounth/mounth_04.png' },
-            { id: 4, item: 'src/assets/mounth/mounth_05.png' },
-            { id: 5, item: 'src/assets/mounth/mounth_06.png' },
-            { id: 6, item: 'src/assets/mounth/mounth_07.png' },
-            { id: 7, item: 'src/assets/mounth/mounth_08.png' },
+            { id: 0, item: imgMounth_01 },
+            { id: 1, item: imgMounth_02 },
+            { id: 2, item: imgMounth_03 },
+            { id: 3, item: imgMounth_04 },
+            { id: 4, item: imgMounth_05 },
+            { id: 5, item: imgMounth_06 },
+            { id: 6, item: imgMounth_07 },
+            { id: 7, item: imgMounth_08 },
         ],
         eyes: [
-            { id: 0, item: 'src/assets/eyes/eyes_01.png' },
-            { id: 1, item: 'src/assets/eyes/eyes_02.png' },
-            { id: 2, item: 'src/assets/eyes/eyes_03.png' },
-            { id: 3, item: 'src/assets/eyes/eyes_04.png' },
-            { id: 4, item: 'src/assets/eyes/eyes_05.png' },
-            { id: 5, item: 'src/assets/eyes/eyes_06.png' },
-            { id: 6, item: 'src/assets/eyes/eyes_07.png' },
+            { id: 0, item: imgEyes_01 },
+            { id: 1, item: imgEyes_02 },
+            { id: 2, item: imgEyes_03 },
+            { id: 3, item: imgEyes_04 },
+            { id: 4, item: imgEyes_05 },
+            { id: 5, item: imgEyes_06 },
+            { id: 6, item: imgEyes_07 },
         ],
         head: [
-            { id: 0, item: 'src/assets/head/head_01.png' },
-            { id: 1, item: 'src/assets/head/head_02.png' },
-            { id: 2, item: 'src/assets/head/head_03.png' },
-            { id: 3, item: 'src/assets/head/head_04.png' },
-            { id: 4, item: 'src/assets/head/head_05.png' },
-            { id: 5, item: 'src/assets/head/head_06.png' },
-            { id: 6, item: 'src/assets/head/head_07.png' },
-            { id: 7, item: 'src/assets/head/head_08.png' },
-            { id: 8, item: 'src/assets/head/head_09.png' },
-            { id: 9, item: 'src/assets/head/head_10.png' },
-            { id: 10, item: 'src/assets/head/head_11.png' },
-            { id: 11, item: 'src/assets/head/head_12.png' },
-            { id: 12, item: 'src/assets/head/head_13.png' },
-            { id: 13, item: 'src/assets/head/head_14.png' },
-            { id: 14, item: 'src/assets/head/head_15.png' },
-            { id: 15, item: 'src/assets/head/head_16.png' },
-            { id: 16, item: 'src/assets/head/head_17.png' },
-            { id: 17, item: 'src/assets/head/head_18.png' },
-            { id: 18, item: 'src/assets/head/head_19.png' },
-            { id: 19, item: 'src/assets/head/head_20.png' },
-            { id: 20, item: 'src/assets/head/head_21.png' },
-            { id: 21, item: 'src/assets/head/head_22.png' },
+            { id: 0, item: imgHead_01 },
+            { id: 1, item: imgHead_02 },
+            { id: 2, item: imgHead_03 },
+            { id: 3, item: imgHead_04 },
+            { id: 4, item: imgHead_05 },
+            { id: 5, item: imgHead_06 },
+            { id: 6, item: imgHead_07 },
+            { id: 7, item: imgHead_08 },
+            { id: 8, item: imgHead_09 },
+            { id: 9, item: imgHead_10 },
+            { id: 10, item: imgHead_11 },
+            { id: 11, item: imgHead_12 },
+            { id: 12, item: imgHead_13 },
+            { id: 13, item: imgHead_14 },
+            { id: 14, item: imgHead_15 },
+            { id: 15, item: imgHead_16 },
+            { id: 16, item: imgHead_17 },
+            { id: 17, item: imgHead_18 },
+            { id: 18, item: imgHead_19 },
+            { id: 19, item: imgHead_20 },
+            { id: 20, item: imgHead_21 },
+            { id: 21, item: imgHead_22 },
         ],
     },
     exemples: [
-        'src/assets/ex_01.png',
-        'src/assets/ex_02.png',
-        'src/assets/ex_03.png',
-        'src/assets/ex_04.png',
-        'src/assets/ex_05.png',
-        'src/assets/ex_06.png',
+        imgExamples_01,
+        imgExamples_02,
+        imgExamples_03,
+        imgExamples_04,
+        imgExamples_05,
     ]
 }
